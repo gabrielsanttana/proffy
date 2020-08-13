1f3f0b15c8a88d1c7c0caefc9c0811c9dad2a9b4
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-08-13 18:57:07 -0300

    Create Teach page structure and styling

--- mobile/src/pages/Teach/index.tsx
+++ mobile/src/pages/Teach/index.tsx
@@ -1,16 +1,29 @@
 import React from 'react';
-import {View, Text, ImageBackground} from 'react-native';
+import {View, Text, ImageBackground, TouchableOpacity} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
 import TeachBackground from '../../assets/images/give-classes-background.png';
 import styles from './styles';
 
 const Teach: React.FC = () => {
+  const {goBack} = useNavigation();
+
   return (
     <View style={styles.container}>
       <ImageBackground
         source={TeachBackground}
         style={styles.content}
         resizeMode="contain"
-      ></ImageBackground>
+      >
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa
+          plataforma web.
+        </Text>
+
+        <TouchableOpacity style={styles.alrightButton} onPress={() => goBack()}>
+          <Text style={styles.alrightButtonText}>Tudo bem</Text>
+        </TouchableOpacity>
+      </ImageBackground>
     </View>
   );
 };
--- mobile/src/pages/Teach/styles.ts
+++ mobile/src/pages/Teach/styles.ts
@@ -12,6 +12,38 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
   },
+
+  title: {
+    fontFamily: 'Archivo_700Bold',
+    color: '#fff',
+    fontSize: 32,
+    lineHeight: 37,
+    maxWidth: 180,
+  },
+
+  description: {
+    marginTop: 24,
+    color: '#d4c2ff',
+    fontSize: 16,
+    lineHeight: 26,
+    fontFamily: 'Poppins_400Regular',
+    maxWidth: 240,
+  },
+
+  alrightButton: {
+    marginVertical: 40,
+    backgroundColor: '#04d361',
+    height: 58,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+  },
+
+  alrightButtonText: {
+    fontFamily: 'Archivo_700Bold',
+    fontSize: 16,
+    color: '#fff',
+  },
 });
 
 export default styles;
