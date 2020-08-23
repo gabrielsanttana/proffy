9629dfe33fc0060e745275954345dc4fa9840d66
gabrielsanttana <gabriel.gsantana7@gmail.com>
2020-08-23 20:29:50 -0300

    Create initial teacher register component structure ant styling

--- frontend/src/pages/TeacherRegister/index.tsx
+++ frontend/src/pages/TeacherRegister/index.tsx
@@ -4,13 +4,32 @@ import './styles.css';
 
 const Register: React.FC = () => {
   return (
-    <div className="teacher-register-container">
-      <div id="teacher-register-content" className="container">
-        <Header
-          title="Que incrível que você quer dar aulas"
-          description="O primeiro passo é preencher esse formulário de inscrição"
-        />
-      </div>
+    <div id="teacher-register-container" className="container">
+      <Header
+        title="Que incrível que você quer dar aulas"
+        description="O primeiro passo é preencher esse formulário de inscrição"
+      />
+
+      <main>
+        <fieldset>
+          <legend>Seus dados</legend>
+
+          <div className="input-box">
+            <label htmlFor="name">Nome completo</label>
+            <input type="text" id="name" autoFocus />
+          </div>
+
+          <div className="input-box">
+            <label htmlFor="avatar">Avatar</label>
+            <input type="text" id="avatar" />
+          </div>
+
+          <div className="input-box">
+            <label htmlFor="whatsapp">Whatsapp</label>
+            <input type="text" id="whatsapp" />
+          </div>
+        </fieldset>
+      </main>
     </div>
   );
 };
